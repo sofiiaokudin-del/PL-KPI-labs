0d7764042cc36803d10de217a8152bfc21700c01
sofiiaokudin-del <sofiia.o.kudin@gmail.com>
2025-09-19 19:47:19 +0300

    Add files via upload

---
+++ Lesson 2/Завдання 5.py
@@ -0,0 +1,9 @@
+n = int(input("Введіть число: "))
+if n < 2:
+    print(n, "не є простим числом")
+else:
+    for i in range(2, n):
+        if n % i == 0:
+            print(n, "не є простим числом")
+    else:
+        print(n, "є простим числом")
